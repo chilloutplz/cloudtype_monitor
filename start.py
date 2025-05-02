435277049d4e12147c99adeacda2d602a5582007
Uncle Bob <starnew@kakao.com>
2025-05-02 22:43:34 +0900

    로그인페이지 저장

--- start.py
+++ start.py
@@ -38,10 +38,18 @@ def setup_webdriver():
 def login_to_github(driver, github_id, github_password):
     """GitHub 계정으로 로그인합니다."""
     print(f"{datetime.now()} - login_to_github 시작")
-    # driver.get("https://app.cloudtype.io/@unclebob/unclebob:main")
     driver.get("https://app.cloudtype.io/@starnew/unclebob:main")
 
-# 
+    # 페이지 소스를 저장
+    try:
+        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+        file_path = f"page_source_{timestamp}.html"
+        with open(file_path, "w", encoding="utf-8") as file:
+            file.write(driver.page_source)
+        print(f"{datetime.now()} - 페이지 소스가 {file_path} 파일에 저장되었습니다.")
+    except Exception as save_error:
+        print(f"{datetime.now()} - 페이지 소스 저장 중 오류 발생: {save_error}")
+
     # JavaScript 렌더링 완료 대기
     try:
         WebDriverWait(driver, 30).until(
@@ -51,7 +59,6 @@ def login_to_github(driver, github_id, github_password):
     except Exception as e:
         print(f"{datetime.now()} - JavaScript 렌더링 대기 중 오류 발생: {e}")
         return
-
 
     # 로그인 버튼 대기
     try:
